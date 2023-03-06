e916c8b2c410a3ff1e133c544c621221828bc1f6
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-03-07 00:57:10 +1100

    update on chapter 1 exercise 1

--- Chapter-1-Programming-Exercises/1-Personal-Information-Exercise.py
+++ Chapter-1-Programming-Exercises/1-Personal-Information-Exercise.py
@@ -3,4 +3,13 @@
 
 # Ask user to input details
 first_name = input("Enter your first name: ")
-last_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+city = input("Enter your address starting with the city: ")
+state = input("State: ")
+zip_code = input("Zip Code: ")
+college_major = input("What's your college major? ")
+
+# Display the personal info to user
+print(f"Name: {first_name + ' ' + last_name}")
+print(f"Address: {city + ', ' + state + ' ' + zip_code}")
+print(f"College Major: {college_major}")
