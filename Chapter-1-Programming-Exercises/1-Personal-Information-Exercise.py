c4a1aa53b2452c4bc57f54812f84a8fa4d9b2e0b
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-03-06 23:54:15 +1100

    initial commit

---
+++ Chapter-1-Programming-Exercises/1-Personal-Information-Exercise.py
@@ -0,0 +1,6 @@
+# Write a program that displays the following information:
+# Name, address, telephone number, college major
+
+# Ask user to input details
+first_name = input("Enter your first name: ")
+last_name = input("Enter your first name: ")
