6cc3a844f7423bddb9ff0a1810a3cc4c340e32ca
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-03-11 23:46:31 +1100

    exercise #5

---
+++ Chapter-2-Programming-Exercises/5-Distance-Traveled-Exercise.py
@@ -0,0 +1,11 @@
+# Assuming there are no accidents or delays, the distance that a car travels down
+# the interstate can be calculated with the following formula: Distance = Speed x Time
+# A car is traveling 70 miles per hour. Write a program that displays the following:
+# The distance the car will travel in 6 hours
+# The distance the car will travel in 10 hours
+# The distance the car will travel in 15 hours
+
+avg_miles = 70
+print(f"The distance the car will travel in 6 hours is approximately {avg_miles * 6} miles.")
+print(f"The distance the car will travel in 10 hours is approximately {avg_miles * 10} miles.")
+print(f"The distance the car will travel in 15 hours is approximately {avg_miles * 15} miles.")
