b70a3859542559bd348ca86ffabe4b65bb0a4441
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-03-17 00:04:24 +1100

    exercise 9

--- Chapter-2-Programming-Exercises/9-Ingredient-Adjuster-Exercise.py
+++ Chapter-2-Programming-Exercises/9-Ingredient-Adjuster-Exercise.py
@@ -5,3 +5,20 @@
 # to make, then displays the number of cups of each ingredient needed
 # for the specified number of cookies.
 
+# Declare variables
+sugar_fixed = 1.5
+butter_fixed = 1
+flour_fixed = 2.75
+cookies_fixed = 48
+
+# User to input desired number of cookies
+user_preference = int(input("Please enter the desired number of cookies: "))
+
+# Computation and display
+sugar_temp = sugar_fixed / cookies_fixed
+butter_temp = butter_fixed / cookies_fixed
+flour_temp = flour_fixed / cookies_fixed
+print(f"You will need the following ingredients to make {user_preference} cookies:")
+print(f"1.{sugar_temp * user_preference: .2f} cup/s of sugar")
+print(f"2.{butter_temp * user_preference: .2f} cup/s of butter")
+print(f"3.{flour_temp * user_preference: .2f} cup/s of flour")
