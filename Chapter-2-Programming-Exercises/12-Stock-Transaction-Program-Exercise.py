57068ab821a27929c6e8472834ec505aed424c59
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-03-27 23:51:38 +1100

    exercise 12

--- Chapter-2-Programming-Exercises/12-Stock-Transaction-Program-Exercise.py
+++ Chapter-2-Programming-Exercises/12-Stock-Transaction-Program-Exercise.py
@@ -12,5 +12,35 @@
 # The amount of commission Joe paid his broker when he bought the stock.
 # The amount for which Joe sold the stock.
 # The amount of commission Joe paid his broker when he sold the stock.
-# Display the amount of moneyu that Joe had left when he sold the stock and paid his broker (both times).
+# Display the amount of money that Joe had left when he sold the stock and paid his broker (both times).
 # If this amount is positive, then Joe made a profit. If the amount is negative, then Joe lost money.
+
+# Purchase
+purchased_shares = float(input("Please enter the number of shares that were purchased: "))
+purchase_price = float(input("Please enter the price paid per share: "))
+purchase_commission = float(input("Please enter the stockbroker commission in percentage: "))
+stockbroker_purchase_commission = (purchased_shares * purchase_price) * (purchase_commission / 100)
+total_cost = (purchased_shares * purchase_price) + stockbroker_purchase_commission
+
+# Sell
+sold_shares = float(input("Please enter the number of shares that were sold: "))
+sell_price = float(input("Please enter the sell price per share: "))
+sell_commission = float(input("Please enter the stockbroker commission in percentage: "))
+stockbroker_sell_commission = (sold_shares * sell_price) * (sell_commission / 100)
+total_sales = (sold_shares * sell_price) - stockbroker_sell_commission
+
+# Calculate profit or loss
+net_amount = total_sales - total_cost
+
+# Display output
+print(f"{stockbroker_purchase_commission} was paid to the broker when the shares were purchased.")
+print(f"{stockbroker_sell_commission} was paid to the broker when the shares were sold.")
+
+# if-else to display profit or loss
+if net_amount > 0:
+    print(f"Profit: ${net_amount}")
+elif net_amount == 0:
+    print("Breakeven")
+else:
+    print(f"Loss: ${net_amount}")
+
