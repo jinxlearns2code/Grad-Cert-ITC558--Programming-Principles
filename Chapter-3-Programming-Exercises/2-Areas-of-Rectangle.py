af6a74d5a5391b890486d9e6518bf756e28c6e4b
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-03-31 23:57:42 +1100

    chapter 3 exercise 2

--- Chapter-3-Programming-Exercises/2-Areas-of-Rectangle.py
+++ Chapter-3-Programming-Exercises/2-Areas-of-Rectangle.py
@@ -4,3 +4,21 @@
 # rectangle has the greater area, or if the areas are
 # the same.
 
+# Ask user to input details
+length1 = float(input("Please enter rectangle 1's length: "))
+width1 = float(input("Please enter rectangle 1's width: "))
+length2 = float(input("Please enter rectangle 2's length: "))
+width2 = float(input("Please enter rectangle 2's width: "))
+
+# Calculation
+rectangle1_area = length1 * width1
+rectangle2_area = length2 * width2
+
+# Display output
+print("")
+if rectangle1_area > rectangle2_area:
+    print("Rectangle 1 has the greater area.")
+elif rectangle2_area > rectangle1_area:
+    print("Rectangle 2 has the greater area.")
+else:
+    print("Both areas are equal.")
