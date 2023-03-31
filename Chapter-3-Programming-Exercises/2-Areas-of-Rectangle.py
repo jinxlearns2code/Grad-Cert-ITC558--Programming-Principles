662f9ed59e60ebc05b4eec4be4b713fb5218c980
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-03-31 23:51:11 +1100

    chapter 3 exercise 2 instructions

---
+++ Chapter-3-Programming-Exercises/2-Areas-of-Rectangle.py
@@ -0,0 +1,6 @@
+# The area of a rectangle's length is times its width.
+# Write a program that asks for the length and width of
+# two rectangles. The program should tell the user which
+# rectangle has the greater area, or if the areas are
+# the same.
+
