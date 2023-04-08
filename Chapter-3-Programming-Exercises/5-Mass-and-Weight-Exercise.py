a729db2c6e531a3415bd43016023db8d19742f1b
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-04-08 23:57:41 +1000

    exercise 5 instructions

---
+++ Chapter-3-Programming-Exercises/5-Mass-and-Weight-Exercise.py
@@ -0,0 +1,4 @@
+# Write a program that asks a user to enter an object's mass, then calculates its weight.
+# If the object weighs more than 500 newtons, display a message indicating that it is too heavy.
+# If the object weighs less than 100 newtons, display a message indicating that it is too light.
+
