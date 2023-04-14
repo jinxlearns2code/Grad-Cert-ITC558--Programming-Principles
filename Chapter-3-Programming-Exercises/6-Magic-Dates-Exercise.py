2184ed8f3b5ade4c5bef18c7725dfba92ccb9512
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-04-14 23:55:53 +1000

    exercise 6 instructions

---
+++ Chapter-3-Programming-Exercises/6-Magic-Dates-Exercise.py
@@ -0,0 +1,5 @@
+# Design a program that asks the user to enter a month (in numeric form), a day, and a two-digit year.
+# The program should then determine whether the month times the day equals the year. If so, it should
+# display a message saying the date is magic. Otherwise, it should display a message saying the date
+# is not magic.
+
