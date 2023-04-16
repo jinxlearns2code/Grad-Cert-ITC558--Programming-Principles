a17a86a7457697181354026474d6fe81bf8ce2da
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-04-16 23:46:17 +1000

    exercise 6

--- Chapter-3-Programming-Exercises/6-Magic-Dates-Exercise.py
+++ Chapter-3-Programming-Exercises/6-Magic-Dates-Exercise.py
@@ -3,3 +3,16 @@
 # display a message saying the date is magic. Otherwise, it should display a message saying the date
 # is not magic.
 
+# Ask users to input data
+day = int(input("Please enter the day of the month: "))
+month = int(input("Please enter the month (in numeric form): "))
+year = int(input("Please enter the year (in 2-digit format e.g. 61 instead of 1961): "))
+
+# Calculate
+check_magic = day * month
+
+# Display output
+if check_magic == year:
+    print("The date is magic.")
+else:
+    print("The date is not magic.")
