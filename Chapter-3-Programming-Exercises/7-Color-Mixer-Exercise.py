be47170a8f48b0aa17193fb010bbb8bde96c920c
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-04-17 23:55:56 +1000

    exercise 7 instructions

---
+++ Chapter-3-Programming-Exercises/7-Color-Mixer-Exercise.py
@@ -0,0 +1,4 @@
+# Design a program that prompts the user to enter the names of two primary colors to mix.
+# If the user enters anything other than "red", "blue", or "yellow", the program should
+# display an error message. Otherwise, the program should display the name of the secondary
+# color that results.
