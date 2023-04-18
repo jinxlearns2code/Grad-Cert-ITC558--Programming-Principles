e21dc31c88f595dbcdf2099cd10000708d193020
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-04-18 23:55:41 +1000

    update

--- Chapter-3-Programming-Exercises/7-Color-Mixer-Exercise.py
+++ Chapter-3-Programming-Exercises/7-Color-Mixer-Exercise.py
@@ -2,3 +2,8 @@
 # If the user enters anything other than "red", "blue", or "yellow", the program should
 # display an error message. Otherwise, the program should display the name of the secondary
 # color that results.
+
+# Ask user for input
+color1 = input("Please enter a color. Choose one from red, blue, or yellow.")
+color2 = input("Please enter another color. Choose one from red, blue, or yellow.")
+
