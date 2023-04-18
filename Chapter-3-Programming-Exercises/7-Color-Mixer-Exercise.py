11e92e3c6886702194f248eb46b4c8b08a8f641f
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-04-19 00:07:59 +1000

    exercise 7

--- Chapter-3-Programming-Exercises/7-Color-Mixer-Exercise.py
+++ Chapter-3-Programming-Exercises/7-Color-Mixer-Exercise.py
@@ -4,6 +4,27 @@
 # color that results.
 
 # Ask user for input
-color1 = input("Please enter a color. Choose one from red, blue, or yellow.")
-color2 = input("Please enter another color. Choose one from red, blue, or yellow.")
+color1 = input("Please enter a color. Choose one from red, blue, or yellow: ")
+color2 = input("Please enter another color. Choose one from red, blue, or yellow: ")
 
+# Display output
+if color1 == "red" and color2 == "red":
+    print(f"{color1} + {color2} = red")
+elif color1 == "blue" and color2 == "blue":
+    print(f"{color1} + {color2} = blue")
+elif color1 == "yellow" and color2 == "yellow":
+    print(f"{color1} + {color2} = yellow")
+elif color1 == "red" and color2 == "blue":
+    print(f"{color1} + {color2} = purple")
+elif color1 == "blue" and color2 == "red":
+    print(f"{color1} + {color2} = purple")
+elif color1 == "blue" and color2 == "yellow":
+    print(f"{color1} + {color2} = green")
+elif color1 == "yellow" and color2 == "blue":
+    print(f"{color1} + {color2} = green")
+elif color1 == "red" and color2 == "yellow":
+    print(f"{color1} + {color2} = orange")
+elif color1 == "yellow" and color2 == "red":
+    print(f"{color1} + {color2} = orange")
+else:
+    print("Error! Please choose a color (red, blue, or yellow).")
